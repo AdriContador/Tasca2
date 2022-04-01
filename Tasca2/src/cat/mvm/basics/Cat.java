c1f8cf4ae39283e51718eb9b9ec2adeda7625bf8
AdriContador <95416860+AdriContador@users.noreply.github.com>
2022-04-01 12:02:55 +0200

    Add files via upload

---
+++ Tasca2/src/cat/mvm/basics/Cat.java
@@ -0,0 +1,18 @@
+package cat.mvm.basics;
+public class Cat {
+    String race;
+    int year;
+
+    public String getRace() {
+        return this.race;
+    }
+    public void setRace(String race) {
+        this.race = race;
+    }
+    public int getYear() {
+        return this.year;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
---
+++ Tasca2/src/cat/mvm/basics/Square.java
@@ -0,0 +1,24 @@
+package cat.mvm.basics;
+public class Square {
+    String name,kind;
+    float measures;
+
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getKind() {
+        return this.kind;
+    }
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+    public float getMeasures() {
+        return this.measures;
+    }
+    public void setMeasures(float measures) {
+        this.measures = measures;
+    }
+}
